5d6b2229744afd52e6a974f2b46484e7a03b9e41
Jeong Seong Cheol <55468858+lejelly@users.noreply.github.com>
2025-04-17 00:25:09 +0900

    change model

--- day1/03_FastAPI/app.py
+++ day1/03_FastAPI/app.py
@@ -13,7 +13,8 @@ from pyngrok import ngrok
 
 # --- 設定 ---
 # モデル名を設定
-MODEL_NAME = "google/gemma-2-2b-jpn-it"  # お好みのモデルに変更可能です
+#MODEL_NAME = "google/gemma-2-2b-jpn-it"
+MODEL_NAME = "microsoft/phi-1"
 print(f"モデル名を設定: {MODEL_NAME}")
 
 # --- モデル設定クラス ---
